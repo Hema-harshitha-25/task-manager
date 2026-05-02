29c3739816e60adb90f605df78094fcd992866c2
Hema Harshitha <hemaharshitha65@gmail.com>
2026-05-02 14:47:52 +0530

    fix app.js path

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 console.log("ENV CHECK:", process.env.MONGO_URI);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
