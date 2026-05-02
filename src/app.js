abdcd6f004fdefd2659dbd1797321d15d5cffeaa
Hema Harshitha <hemaharshitha65@gmail.com>
2026-05-02 13:55:36 +0530

    fix app.js path

--- src/app.js
+++ src/app.js
@@ -1,33 +1,30 @@
-export default App;
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
-
 const connectDB = require("./config/database");
-const userRoutes = require("./routes/UserRoutes");
-const taskRoutes = require("./routes/TaskRoutes");
 
 const app = express();
 
-// Middleware
-app.use(cors());
+// connect database
+connectDB();
+
+// middleware
 app.use(express.json());
 
-// Test route (IMPORTANT)
+// test route
 app.get("/", (req, res) => {
-  res.send("API is working 🚀");
+  res.send("API is running...");
 });
 
-// Routes
+// routes
+const userRoutes = require("./routes/UserRoutes");
+const taskRoutes = require("./routes/TaskRoutes");
+
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Connect DB
-connectDB();
-
-// PORT FIX (VERY IMPORTANT)
-const PORT = process.env.PORT || 5000;
+// PORT
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
