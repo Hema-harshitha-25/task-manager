229f4bb379e06c400ad4e339d1b5802a1bce1662
Hema Harshitha <hemaharshitha65@gmail.com>
2026-05-02 13:20:20 +0530

    fix start script for railway

--- src/app.js
+++ src/app.js
@@ -1,26 +1,33 @@
 require("dotenv").config();
 
 const express = require("express");
-const connectDB = require("./config/database");
+const cors = require("cors");
 
+const connectDB = require("./config/database");
 const userRoutes = require("./routes/UserRoutes");
 const taskRoutes = require("./routes/TaskRoutes");
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
-// Connect DB
-connectDB();
+// Test route (IMPORTANT)
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Server
+// Connect DB
+connectDB();
+
+// PORT FIX (VERY IMPORTANT)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
