0715b02486bd26329eefc1aa8b6a136e227701f7
Hema Harshitha <hemaharshitha65@gmail.com>
2026-05-02 13:16:47 +0530

    fix railway port

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
