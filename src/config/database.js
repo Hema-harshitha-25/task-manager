8154113bf3fcf6070f8d76beedfee291c4844c19
Hema Harshitha <hemaharshitha65@gmail.com>
2026-05-02 13:58:18 +0530

    fix app.js path

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`;
+    console.log("Connecting with URI:", process.env.MONGO_URI);
 
-    console.log("Connecting with URI:", uri); // DEBUG
+    await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(uri);
-
-    console.log("MongoDB connected");
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error(error);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
